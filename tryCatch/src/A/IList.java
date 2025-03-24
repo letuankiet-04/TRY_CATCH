e6713a15f461d2f59dcc81599ed251e9c4d94d0d
Lê tuấn Kiệt <lkiet2404.2005@gmail.com>
2025-03-24 13:02:18 +0700

    Update IList.java

--- tryCatch/src/A/IList.java
+++ tryCatch/src/A/IList.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package A;
 
-/**
- *
- * @author KIET-PC
- */
 public interface IList {
-    
+    boolean add(Window x);
+    Window search(String id);
+    void printAll();
 }
